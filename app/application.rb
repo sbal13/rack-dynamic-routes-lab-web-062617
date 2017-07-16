4fe8d30e6de1abf3e0b8e7c94750a3796f6a0d94
sbal13 <sbal13@gmail.com>
2017-07-16 09:24:39 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+
+	def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+    	term = req.path.split("/items/").last
+		found = Item.items.find{|item| item.name == term}
+		if found
+			resp.write found.price
+		else
+			resp.write "Item not found"
+			resp.status = 400
+		end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
+
+end
